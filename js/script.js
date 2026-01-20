20c17613a7b42e3ad7e021451700646ead136a02
tigran-sargsyan-w <tigran.sargsyan.w@gmail.com>
2026-01-20 20:59:34 +0000

    Deploying to gh-pages from @ tigran-sargsyan-w/tigran-sargsyan-w.github.io@c402016a4d3865163fb77b3349a611650ccf9699 🚀

--- js/script.js
+++ js/script.js
@@ -1,73 +1,77 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+(() => {
+  "use strict";
 
-hamburger.addEventListener("click", mobileMenu);
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector(".nav-menu");
 
-function mobileMenu() {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-}
+  hamburger.addEventListener("click", mobileMenu);
 
-// Close navbar when link is clicked
-const navLink = document.querySelectorAll(".nav-link");
-
-navLink.forEach((n) => n.addEventListener("click", closeMenu));
-
-function closeMenu() {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
-}
-
-// Event Listeners: Handling toggle event
-const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-const userIcon = document.getElementById("user-icon");
-const downloadIcon = document.getElementById("download-icon");
-
-function switchTheme(e) {
-  if (e.target.checked) {
-    document.documentElement.setAttribute("data-theme", "dark");
-    userIcon.src = "./assets/user_dark.png";
-    if (downloadIcon !== null) {
-      downloadIcon.src = "./assets/download-dark.png";
-    }
-    localStorage.setItem("theme", "dark");
-  } else {
-    document.documentElement.setAttribute("data-theme", "light");
-    userIcon.src = "./assets/user_light.png";
-    if (downloadIcon !== null) {
-      downloadIcon.src = "./assets/download-light.png";
-    }
-
-    localStorage.setItem("theme", "light");
+  function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
   }
-}
 
-toggleSwitch.addEventListener("change", switchTheme);
+  // Close navbar when link is clicked
+  const navLink = document.querySelectorAll(".nav-link");
 
-// Save user preference on load
-const currentTheme = localStorage.getItem("theme");
+  navLink.forEach((n) => n.addEventListener("click", closeMenu));
 
-if (currentTheme) {
-  document.documentElement.setAttribute("data-theme", currentTheme);
+  function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  }
 
-  if (currentTheme === "dark") {
-    toggleSwitch.checked = true;
-    userIcon.src = "./assets/user_dark.png";
-    if (downloadIcon !== null) {
-      downloadIcon.src = "./assets/download-dark.png";
-    }
-  } else {
-    userIcon.src = "./assets/user_light.png";
-    if (downloadIcon !== null) {
-      downloadIcon.src = "./assets/download-light.png";
+  // Event Listeners: Handling toggle event
+  const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+  const userIcon = document.getElementById("user-icon");
+  const downloadIcon = document.getElementById("download-icon");
+
+  function switchTheme(e) {
+    if (e.target.checked) {
+      document.documentElement.setAttribute("data-theme", "dark");
+      userIcon.src = "./assets/user_dark.png";
+      if (downloadIcon !== null) {
+        downloadIcon.src = "./assets/download-dark.png";
+      }
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.setAttribute("data-theme", "light");
+      userIcon.src = "./assets/user_light.png";
+      if (downloadIcon !== null) {
+        downloadIcon.src = "./assets/download-light.png";
+      }
+
+      localStorage.setItem("theme", "light");
     }
   }
-}
 
+  toggleSwitch.addEventListener("change", switchTheme);
 
-//Adding date
+  // Save user preference on load
+  const currentTheme = localStorage.getItem("theme");
 
-let myDate = document.querySelector("#datee");
+  if (currentTheme) {
+    document.documentElement.setAttribute("data-theme", currentTheme);
 
-const yes = new Date().getFullYear();
-myDate.innerHTML = yes;
+    if (currentTheme === "dark") {
+      toggleSwitch.checked = true;
+      userIcon.src = "./assets/user_dark.png";
+      if (downloadIcon !== null) {
+        downloadIcon.src = "./assets/download-dark.png";
+      }
+    } else {
+      userIcon.src = "./assets/user_light.png";
+      if (downloadIcon !== null) {
+        downloadIcon.src = "./assets/download-light.png";
+      }
+    }
+  }
+
+  //Adding date
+  const myDate = document.querySelector("#datee");
+
+  if (myDate) {
+    const yes = new Date().getFullYear();
+    myDate.innerHTML = yes;
+  }
+})();
